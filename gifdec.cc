83e38c1e554d40c76c2c5c3d65f87f169a5c9929
lqt0223 <lqt19920223@126.com>
2023-07-21 21:16:04 +0800

    fixes

--- gifdec.cc
+++ gifdec.cc
@@ -9,17 +9,17 @@
 using namespace std;
 
 char buf[256];
-map<uint16_t, string> code_table;
+map<uint16_t, u8string> code_table;
 typedef struct {
     uint8_t cr;
     const string& gct;
     string& framebuffer; // rgb packed
 } dec_ctx_t;
 
-void init_code_table(uint8_t init_table_size) {
+void init_code_table(uint16_t init_table_size) {
     code_table.clear();
-    for (uint8_t i = 0; i < init_table_size; i++) {
-        code_table[code_table.size()] = string(1, i);
+    for (uint16_t i = 0; i < init_table_size; i++) {
+        code_table[code_table.size()] = u8string(1, i);
     }
 }
 
@@ -51,9 +51,10 @@ void lzw_unpack_decode(ifstream& file, dec_ctx_t ctx) {
     uint16_t clear_code = 1 << min_code_size;
     uint16_t end_code = clear_code + 1;
 
-    init_code_table(1 << min_code_size);
-    code_table[clear_code] = string(1, clear_code);
-    code_table[end_code] = string(1, end_code);
+    uint16_t table_size = 1 << min_code_size;
+    init_code_table(table_size);
+    code_table[clear_code] = u8string(1, clear_code);
+    code_table[end_code] = u8string(1, end_code);
 
     uint8_t code_size = min_code_size + 1;
     uint32_t n_bit = 0;
@@ -103,7 +104,7 @@ void lzw_unpack_decode(ifstream& file, dec_ctx_t ctx) {
 
         // lzw decoding
         if (code_table.contains(code)) {
-            string indexes = code_table[code];
+            u8string indexes = code_table[code];
             for (size_t i = 0; i < indexes.size(); i++) {
                 memcpy(fb_ptr, &ctx.gct[indexes[i]*3], 3);
                 fb_ptr += 3;
@@ -111,12 +112,12 @@ void lzw_unpack_decode(ifstream& file, dec_ctx_t ctx) {
             if (prev_code == end_code) {
                 prev_code = code; // todo this case is the 2nd code, should be moved out of loop
             } else {
-                code_table[code_table.size()] = code_table[prev_code] + string(1, indexes[0]);
+                code_table[code_table.size()] = code_table[prev_code] + u8string(1, indexes[0]);
                 prev_code = code;
             }
         } else {
-            string prev_indexes = code_table[prev_code];
-            string new_indexes = prev_indexes + string(1, prev_indexes[0]);
+            u8string prev_indexes = code_table[prev_code];
+            u8string new_indexes = prev_indexes + u8string(1, prev_indexes[0]);
             for (size_t i = 0; i < new_indexes.size(); i++) {
                 memcpy(fb_ptr, &ctx.gct[new_indexes[i]*3], 3);
                 fb_ptr += 3;
@@ -171,10 +172,10 @@ int main(int argc, char** argv) {
 
     // framebuffer
     string framebuffer(lsd.w * lsd.h * 3, ' ');
-// Netscape Looping Application Extension todo this block is optional
-    // read_assert_str_equal(file, "\x21\xff\x0b", "netscape looping application extension header error");
-    // read_assert_str_equal(file, "NETSCAPE2.0", "netscape looping application extension identifier error");
-    // read_assert_str_equal(file, "\x03\x01\x00\x00\x00", "netscape looping application extension content error");
+    // Netscape Looping Application Extension todo this block is optional
+    read_assert_str_equal(file, "\x21\xff\x0b", "netscape looping application extension header error");
+    read_assert_str_equal(file, "NETSCAPE2.0", "netscape looping application extension identifier error");
+    read_assert_str_equal(file, "\x03\x01\x00\x00\x00", "netscape looping application extension content error");
 
     // while (file.peek() != 0x3b) {
     dec_ctx_t ctx = { lsd.packed.cr, gct, framebuffer };
@@ -183,7 +184,7 @@ int main(int argc, char** argv) {
     // draw decoded framebuffer with sdl
     // todo sdl error handling
     SDL_Init(SDL_INIT_EVERYTHING);
-    SDL_Window* window = SDL_CreateWindow("gif decoder", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, lsd.w, lsd.h, SDL_WINDOW_OPENGL);
+    SDL_Window* window = SDL_CreateWindow("gif decoder", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, lsd.w, lsd.h, SDL_WINDOW_OPENGL | SDL_WINDOW_RESIZABLE);
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     SDL_Surface* surf = SDL_CreateRGBSurfaceFrom(ctx.framebuffer.data(), lsd.w, lsd.h, 3 * 8, lsd.w * 3, 0x0000FF, 0x00FF00, 0xFF0000, 0);
     SDL_Texture* tex = SDL_CreateTextureFromSurface(renderer, surf);
