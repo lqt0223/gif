348e132227bb1104ef2844a27a8954bb1e4b3983
lqt0223 <lqt19920223@126.com>
2023-08-30 20:52:05 +0800

    temp 444 component-3

--- jpegenc.cc
+++ jpegenc.cc
@@ -153,8 +153,8 @@ void JpegEncoder::output_sof() {
     write_u16_be(this->h); // height first
     write_u16_be(this->w);
     printf("\x03"); // 3-component
-    // 420 sampling
-    fwrite("\x01\x22\x00", 1, 3, stdout); 
+    // 444 sampling
+    fwrite("\x01\x11\x00", 1, 3, stdout); 
     fwrite("\x02\x11\x01", 1, 3, stdout);
     fwrite("\x03\x11\x01", 1, 3, stdout);
 }
@@ -163,7 +163,7 @@ void JpegEncoder::output_sos() {
     printf("\xff\xda");
     write_u16_be(12); // for 3-component jpeg
     printf("\x03"); // 3-component
-    // 420 sampling
+    // 444 sampling
     fwrite("\x01\x00", 1, 2, stdout); 
     printf("\x02\x11");
     printf("\x03\x11");
@@ -256,8 +256,8 @@ int JpegEncoder::encode_8x8_per_component(
 ) {
     memset(temp1, 0, 64);
     memset(temp2, 0, 64);
-    // 420 sampling
-    uint8_t mcu_w = 16;
+    // 444 sampling
+    uint8_t mcu_w = 8;
     fill_8x8(src_buffer, temp1, x, y, sample_h, sample_v, mcu_w);
     dct_8x8(temp1, temp2);
     quantize_8x8(temp2, temp1, is_chroma ? qt_chroma : qt_luma);
@@ -289,8 +289,8 @@ int JpegEncoder::encode_8x8_per_component(
 }
 
 void JpegEncoder::output_encoded_image_data() {
-    // 420 sampling
-    uint8_t mcu_w = 16, mcu_h = 16;
+    // 444 sampling
+    uint8_t mcu_w = 8, mcu_h = 8;
 
     char* Y_MCU = new char[mcu_w*mcu_h];
     char* Cb_MCU = new char[mcu_w*mcu_h];
@@ -311,16 +311,10 @@ void JpegEncoder::output_encoded_image_data() {
 
             // Y1 top left
             dc_y = this->encode_8x8_per_component(Y_MCU, 0, 0, 1, 1, false, dc_y);
-            // Y2 top right
-            dc_y = this->encode_8x8_per_component(Y_MCU, 8, 0, 1, 1, false, dc_y);
-            // Y3 bottom left
-            dc_y = this->encode_8x8_per_component(Y_MCU, 0, 8, 1, 1, false, dc_y);
-            // Y4 bottom right
-            dc_y = this->encode_8x8_per_component(Y_MCU, 8, 8, 1, 1, false, dc_y);
             // Cb
-            dc_cb = this->encode_8x8_per_component(Cb_MCU, 0, 0, 2, 2, true, dc_cb);
+            dc_cb = this->encode_8x8_per_component(Cb_MCU, 0, 0, 1, 1, true, dc_cb);
             // Cr
-            dc_cr = this->encode_8x8_per_component(Cr_MCU, 0, 0, 2, 2, true, dc_cr);
+            dc_cr = this->encode_8x8_per_component(Cr_MCU, 0, 0, 1, 1, true, dc_cr);
         }
     }
     std::cout << this->bitstream.store;
